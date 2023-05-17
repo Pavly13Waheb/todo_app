70e7485797cbb57bb4e952419afc8621d3ae7b1c
Pavly13Waheb <130704865+Pavly13Waheb@users.noreply.github.com>
2023-05-17 16:46:56 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
@@ -5,9 +6,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo/settings_provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:todo/theme/app_theme.dart';
+import 'firebase_options.dart';
 import 'home_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(ChangeNotifierProvider(
       create: (_) => SettingsProvider(), child: MyApp()));
 }
